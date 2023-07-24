3ddd34dcaeef83d59170b416f1cfce0f972d6a31
Tushar Pokle <tushar.pokle@gmail.com>
2023-07-24 10:53:30 +1000

    Increase memory limits to 16G

--- sdpro.sh
+++ sdpro.sh
@@ -3,6 +3,9 @@
 # Runs pyspark shell with delta support enabled
 
 poetry run pyspark \
+    --driver-memory 16G \
+    --executor-memory 16G \
     --packages io.delta:delta-core_2.12:2.2.0 \
+    --conf "spark.driver.maxResultSize=16G" \
     --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
-    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" $*
